69c45f8f64efbbc98ed0a7392ca8b2016f20f1e1
R. Allan White <allan@catalyze.io>
2016-06-22 10:45:47 -0700

    Basic concat of minified bower packages' js files.

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,17 @@ var Promise = require('es6-promise').Promise;
 // The above was necessary to fix compile-time errors: http://stackoverflow.com/a/35229818
 var gulp = require('gulp');
 var $    = require('gulp-load-plugins')();
+var concat = require('gulp-concat');
 
 var sassPaths = [
   'bower_components/foundation-sites/scss',
   'bower_components/motion-ui/src'
+];
+var jsPaths = [
+    'bower_components/jquery/dist/jquery.min.js',
+    'bower_components/what-input/what-input.min.js',
+    'bower_components/foundation-sites/dist/foundation.min.js',
+    'bower_components/svg-injector/dist/svg-injector.min.js'
 ];
 
 gulp.task('sass', function() {
@@ -18,6 +25,12 @@ gulp.task('sass', function() {
       browsers: ['last 2 versions', 'ie >= 9']
     }))
     .pipe(gulp.dest('css'));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src(jsPaths)
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('./js/dist/'));
 });
 
 gulp.task('default', ['sass'], function() {
